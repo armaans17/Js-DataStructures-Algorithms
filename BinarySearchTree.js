28c98a44a4cef9e5720899325d13ce92b679da9b
Armaan <armaanmaani@gmail.com>
2021-04-01 22:31:42 -0400

    BST remove

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,24 +1,24 @@
 class Node {
-  constructor(node, left, right) {
+  constructor(node) {
     this.value = node;
-    this.left = left;
-    this.right = right;
+    this.left = null;
+    this.right = null;
   }
 }
 
 class BinarySearchTree {
   constructor() {
-    this.head = null;
+    this.root = null;
   }
 
   insert(node) {
     const newNode = new Node(node);
 
-    if (!this.head) {
-      this.head = newNode;
+    if (!this.root) {
+      this.root = newNode;
       return;
     }
-    let currentNode = this.head;
+    let currentNode = this.root;
     while (currentNode) {
       if (newNode.value < currentNode.value) {
         if (!currentNode.left) {
@@ -39,9 +39,9 @@ class BinarySearchTree {
   }
 
   lookup(node) {
-    if (!this.head) return false;
+    if (!this.root) return false;
 
-    let currentNode = this.head;
+    let currentNode = this.root;
     while (currentNode) {
       if (currentNode.value === node) {
         return true;
@@ -54,7 +54,82 @@ class BinarySearchTree {
     return false;
   }
 
-  remove() {}
+  remove(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    let parentNode = null;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        //We have a match, get to work!
+
+        //Option 1: No right child:
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            //if parent > current value, make current left child a child of parent
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+
+              //if parent < current value, make left child a right child of parent
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+
+          //Option 2: Right child which doesnt have a left child
+        } else if (!currentNode.right.left) {
+          currentNode.right.left = currentNode.left;
+          if (parentNode === null) {
+            this.root = currentNode.right;
+          } else {
+            //if parent > current, make right child of the left the parent
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+
+              //if parent < current, make right child a right child of the parent
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right;
+            }
+          }
+
+          //Option 3: Right child that has a left child
+        } else {
+          //find the Right child's left most child
+          let leftmost = currentNode.right.left;
+          let leftmostParent = currentNode.right;
+          while (leftmost.left !== null) {
+            leftmostParent = leftmost;
+            leftmost = leftmost.left;
+          }
+
+          //Parent's left subtree is now leftmost's right subtree
+          leftmostParent.left = leftmost.right;
+          leftmost.left = currentNode.left;
+          leftmost.right = currentNode.right;
+
+          if (parentNode === null) {
+            this.root = leftmost;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = leftmost;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = leftmost;
+            }
+          }
+        }
+        return true;
+      }
+    }
+  }
 }
 
 const Tree1 = new BinarySearchTree();
@@ -69,3 +144,6 @@ Tree1.insert(14);
 //  5
 Tree1.lookup(11); // false
 Tree1.lookup(14); // true
+console.log(Tree1);
+console.log(Tree1.remove(9));
+console.log(Tree1);
